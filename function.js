9e96ade8553f4b26a41f058e0e90cd102b6f63b3
technikboy04 <jan.sniehota@gmx.de>
2020-05-27 14:57:30 +0200

    If Abfrage ob Data Variable leer ist

--- function.js
+++ function.js
@@ -9,8 +9,19 @@ const CHECK = "fa-check-circle";
 const UNCHECK = "fa-circle-thin";
 const LINE_THROUGH = "lineThrough";
 
-// Variables
+// Variablen
 let LIST, id;
 
-// get item from localstorage
+// get item von localstorage
 let data = localStorage.getItem("TODO");
+
+// Pruefe ob data nicht leer ist
+if(data){
+    LIST = JSON.parse(data);
+    id = LIST.length; // setzte id auf den letzten in der Liste
+    loadList(LIST); // lade die Liste in das aktuelle Interface
+}else{
+    // Wenn data nicht leer ist
+    LIST = [];
+    id = 0;
+}
