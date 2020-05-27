3400ea2120a325c2958929bfe7ad8b52975d1bf4
technikboy04 <jan.sniehota@gmx.de>
2020-05-27 15:04:27 +0200

    Fuege ein item der Liste hinzu mit if Abfrage

--- function.js
+++ function.js
@@ -64,3 +64,29 @@ function addToDo(toDo, id, done, trash){
     
     list.insertAdjacentHTML(position, item);
 }
+
+
+// Füge ein item der Liste hinzu (user the enter key)
+document.addEventListener("keyup",function(even){
+    if(event.keyCode == 13){
+        const toDo = input.value;
+        
+        // if the input isn't empty
+        if(toDo){
+            addToDo(toDo, id, false, false);
+            
+            LIST.push({
+                name : toDo,
+                id : id,
+                done : false,
+                trash : false
+            });
+            
+            // add item to localstorage ( this code must be added where the LIST array is updated)
+            localStorage.setItem("TODO", JSON.stringify(LIST));
+            
+            id++;
+        }
+        input.value = "";
+    }
+});
