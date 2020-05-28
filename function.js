068a6442b869919eac2e7b6bd4a9597b85a240b9
technikboy04 <jan.sniehota@gmx.de>
2020-05-28 13:38:24 +0200

    erstellen der RGB eingabe

--- function.js
+++ function.js
@@ -53,3 +53,11 @@ function newElement() {
     }
   }
 } 
+
+//Ermöglicht es, die Hintergrundfarbe zu bestimmen
+function rgbfarben() {
+  var rot= document.form.rfeld.value;
+  var gruen = document.form.gfeld.value;
+  var blau = document.form.bfeld.value;
+  document.getElementsByTagName("body")[0].style.backgroundColor = "rgb("+ rot +"%,"+ gruen +"%, " +blau+"%)";
+  }
